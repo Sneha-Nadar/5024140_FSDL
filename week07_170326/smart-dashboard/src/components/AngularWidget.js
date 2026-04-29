4c2bfce63072759cecad3ebc75d9c7fe71cd0e4e
Sneha-Nadar <snehanadar16@gmail.com>
2026-04-29 17:53:52 +0000

    dashboard done

--- week07_170326/smart-dashboard/src/components/AngularWidget.js
+++ week07_170326/smart-dashboard/src/components/AngularWidget.js
@@ -2,32 +2,40 @@ import { useEffect } from "react";
 
 export default function AngularWidget() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js";
+    if (!window.angular) {
+      const script = document.createElement("script");
+      script.src =
+        "https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js";
 
-    script.onload = () => {
-      const app = window.angular.module("myApp", []);
-      app.controller("myCtrl", function ($scope) {
-        $scope.name = "";
-      });
+      script.onload = () => {
+        const app = window.angular.module("myApp", []);
 
-      window.angular.bootstrap(
-        document.getElementById("angular-root"),
-        ["myApp"]
-      );
-    };
+        app.controller("myCtrl", function ($scope) {
+          $scope.name = "";
+        });
 
-    document.body.appendChild(script);
+        window.angular.bootstrap(
+          document.getElementById("angular-root"),
+          ["myApp"]
+        );
+      };
+
+      document.body.appendChild(script);
+    }
   }, []);
 
   return (
-    <div id="angular-root">
-      <div ng-controller="myCtrl">
-        <h2>AngularJS</h2>
-        <input ng-model="name" placeholder="Enter name" />
-        <p>Hello {{name}}</p>
-      </div>
-    </div>
+    <div
+      id="angular-root"
+      dangerouslySetInnerHTML={{
+        __html: `
+          <div ng-controller="myCtrl">
+            <h2>AngularJS</h2>
+            <input ng-model="name" placeholder="Enter name" />
+            <p>Hello {{name}}</p>
+          </div>
+        `,
+      }}
+    />
   );
 }
