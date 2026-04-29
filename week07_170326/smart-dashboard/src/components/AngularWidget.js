12916f08d4261de999adc3d227ff9007805abbff
Sneha-Nadar <snehanadar16@gmail.com>
2026-04-29 17:40:02 +0000

    okkkk

--- week07_170326/smart-dashboard/src/components/AngularWidget.js
+++ week07_170326/smart-dashboard/src/components/AngularWidget.js
@@ -2,20 +2,35 @@ import { useEffect } from "react";
 
 export default function AngularWidget() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js";
-    document.body.appendChild(script);
+    if (!window.angular) {
+      const script = document.createElement("script");
+      script.src =
+        "https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js";
+
+      script.onload = () => {
+        const app = window.angular.module("myApp", []);
+
+        app.controller("myCtrl", function ($scope) {
+          $scope.name = "";
+        });
+
+        window.angular.bootstrap(
+          document.getElementById("angular-root"),
+          ["myApp"]
+        );
+      };
+
+      document.body.appendChild(script);
+    }
   }, []);
 
   return (
-    <div dangerouslySetInnerHTML={{
-      __html: `
-      <div ng-app="">
+    <div id="angular-root">
+      <div ng-controller="myCtrl">
         <h2>AngularJS Live</h2>
-        <input ng-model="name" placeholder="Enter name">
+        <input ng-model="name" placeholder="Enter name" />
         <p>Hello {{name}}</p>
       </div>
-      `
-    }} />
+    </div>
   );
 }
