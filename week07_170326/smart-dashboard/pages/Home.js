cea891405f70434da4fa48cbc50017592db76e9a
Sneha-Nadar <snehanadar16@gmail.com>
2026-04-29 17:23:18 +0000

    okk

---
+++ week07_170326/smart-dashboard/pages/Home.js
@@ -0,0 +1,10 @@
+//import AngularWidget from "../components/AngularWidget";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome 🚀</h1>
+      <AngularWidget />
+    </div>
+  );
+}
